2eeb569295a0be711294abf183e87499b70def67
Andreas Perhab <bigbear.ap@gmail.com>
2016-01-12 21:58:23 +0100

    apply apache license

--- src/main/java/com/perhab/image/SVGImageIO.java
+++ src/main/java/com/perhab/image/SVGImageIO.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2016 Andreas "bigbear3001" Perhab
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package com.perhab.image;
 
 import java.awt.Image;
@@ -81,7 +96,7 @@ public class SVGImageIO {
 	private static class SVGTranscoderException extends RuntimeException {
 
 		/**
-		 * unique serilialization version id.
+		 * unique serialization version id.
 		 */
 		private static final long serialVersionUID = 1L;
 
